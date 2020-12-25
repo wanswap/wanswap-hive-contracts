21b88d8927d413778c59e6c6aa85ac13f6bb81bb
Morpheus <morpheus.blockchain@gmail.com>
2020-12-25 11:36:45 +0800

    update deploy info

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,6 +2,6 @@ const wanFarm = artifacts.require("wanFarm");
 
 module.exports = function (deployer) {
   // deployer.deploy(Migrations);
-  deployer.deploy(wanFarm, '0x916283cc60fdaf05069796466af164876e35d21f', '0x916283cc60fdaf05069796466af164876e35d21f');
+  deployer.deploy(wanFarm, '0xdabd997ae5e4799be47d6e69d9431615cba28f48', '0xdabd997ae5e4799be47d6e69d9431615cba28f48');
 };
 
